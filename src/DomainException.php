5436144ecf050ad67d18ca15ba109a2b664c842d
David Dashifen Kees <dashifen@dashifen.com>
2017-05-05 08:45:51 -0400

    added DomainException constants

--- src/DomainException.php
+++ src/DomainException.php
@@ -3,5 +3,15 @@
 namespace Dashifen\Domain;
 
 class DomainException extends \Exception {
-
+	public const UNKNOWN_ERROR = 1;
+	public const UNEXPECTED_BEHAVIOR = 2;
+	
+	public function __construct($message = "", $code = 0, \Throwable $previous = null) {
+		$reflection = new \ReflectionClass($this);
+		if (!in_array($code, $reflection->getConstants())) {
+			$code = self::UNKNOWN_ERROR;
+		}
+		
+		parent::__construct($message, $code, $previous);
+	}
 }
